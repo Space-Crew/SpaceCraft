a8a4607557df92556deefa95f551475a657ea435
Cheryl Ng <cherylngql@gmail.com>
2018-08-14 17:40:04 -0400

    rename plane

--- client/routes.js
+++ client/routes.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
-import {Login, Signup, UserHome, Home, Plane, WorldList} from './components'
+import {Login, Signup, UserHome, Home, Create, WorldList} from './components'
 import PointerLockDemo from './components/PointerLockDemo'
 import {db} from './firebase'
 
@@ -16,8 +16,8 @@ export default class Routes extends Component {
       <Switch>
         {/* Routes placed here are available to all visitors */}
         <Route exact path="/" component={Home} />
-        <Route exact path="/plane" component={Plane} />
-        <Route path="/plane/:id" component={Plane} />
+        <Route exact path="/create" component={Create} />
+        <Route path="/create/:id" component={Create} />
         <Route path="/worlds" component={WorldList} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
