0e1495b120c6f383bb8c2ceb02a931af82a2a8f7
Graham Johnson <grahamscottjohnson@gmail.com>
2018-08-15 20:59:22 -0400

    water flows better

--- client/3d/meshes/water.js
+++ client/3d/meshes/water.js
@@ -2,18 +2,6 @@ import {makeUnitCube} from '.'
 import * as THREE from 'three'
 import {toKey, toPosition} from '..'
 
-export function generateWater(listOfWaterSources, cubes) {
-  const waterCubes = []
-  listOfWaterSources.forEach(waterSource => {
-    makeWaterCubes(waterSource, cubes, waterCubes)
-  })
-  return waterCubes
-}
-
-function makeWaterCubes(waterSource, cubes, waterCubes) {
-  makeWaterCube(waterSource.position, 3, cubes, waterCubes)
-}
-
 export class FlowCube {
   constructor(x = 0, y = 0, z = 0, isSource = false, parents = {}, volume = 1) {
     this.position = {x, y, z}
@@ -22,70 +10,117 @@ export class FlowCube {
     this.children = {}
     this.volume = volume
   }
-  createChild(childPosition, flowMap) {
+  createChild(childPosition, flowMap, shouldDecrementVolume = true) {
     const key = toKey(childPosition)
-    if (!flowMap[key] && this.volume > 1) {
+    const hasVolumeToFlow = shouldDecrementVolume
+      ? this.volume > 1
+      : this.volume > 0
+    if (!flowMap[key] && hasVolumeToFlow) {
       const child = new FlowCube(
         childPosition.x,
         childPosition.y,
         childPosition.z,
         false,
         {[toKey(this.position)]: this},
-        this.volume - 1
+        shouldDecrementVolume ? this.volume - 1 : this.volume
       )
       this.children[key] = child
       flowMap[key] = child
+      return child
     }
+    return null
   }
-  spawnChildren(cubes, flowMap) {}
+  clonePosition() {
+    return {...this.position}
+  }
+  spawnChildren(cubes, flowMap) {
+    const createdChildren = []
+    const oneDown = this.clonePosition()
+    oneDown.y -= 1
+    const shouldFlowDown = !cubes[toKey(oneDown)] && oneDown.y >= -64
+    if (shouldFlowDown) {
+      createdChildren.push(this.createChild(oneDown, flowMap, false))
+    } else {
+      //flow in all directions
+      const adjacentPositions = this.getAdjacentPositions()
+      adjacentPositions.forEach(position => {
+        const shouldFlow = !cubes[toKey(position)]
+        if (shouldFlow) {
+          createdChildren.push(this.createChild(position, flowMap))
+        }
+      })
+    }
+    createdChildren.forEach(child => {
+      if (child) {
+        child.spawnChildren(cubes, flowMap)
+      }
+    })
+  }
+  getAdjacentPositions() {
+    const northPosition = this.clonePosition()
+    northPosition.z += 1
+    const southPosition = this.clonePosition()
+    southPosition.z -= 1
+    const eastPosition = this.clonePosition()
+    eastPosition.x += 1
+    const westPosition = this.clonePosition()
+    westPosition.x -= 1
+
+    const positions = [northPosition, eastPosition, southPosition, westPosition]
+    return positions
+  }
   removeChildren(flowMap) {
-    this.children.forEach(child => {
-      delete flowMap[toKey(child.position)]
+    const children = Object.values(this.children)
+    children.forEach(child => {
+      const parents = child.parents
+      delete parents[toKey(this.position)]
+      delete this.children[toKey(child.position)]
+      if (Object.keys(parents).length === 0) {
+        delete flowMap[toKey(child.position)]
+      }
     })
     this.children = {}
   }
 }
 
-class WaterMap {
-  constructor() {
-    this.positions = {}
-  }
-}
+// export function generateWater(listOfWaterSources, cubes) {
+//   const waterCubes = []
+//   listOfWaterSources.forEach(waterSource => {
+//     makeWaterCubes(waterSource, cubes, waterCubes)
+//   })
+//   return waterCubes
+// }
 
-function makeWaterCube(
-  position,
-  puddleLength, // water should only flow so far
-  cubes,
-  waterCubes
-) {
-  //basecase - block already there, so water can't flow into it
-  // or water is already there
-  // or puddleLength = 0
-  const basecase = puddleLength === 0 || cubes[toKey(position)]
-  if (basecase) {
-    return waterCubes
-  }
-  const cube = makeUnitCube(position, 0x0000ff, 1)
-  cube.name = 'WATER'
-  waterCubes.push(cube)
-  const oneDown = position.clone()
-  oneDown.y -= 1
-  const shouldFlowDown = !cubes[toKey(oneDown)] && oneDown.y > -64
-  if (shouldFlowDown) {
-    makeWaterCube(oneDown, puddleLength, cubes, waterCubes)
-  } else {
-    //flow in all directions
-    const northPosition = position.clone()
-    northPosition.z += 1
-    makeWaterCube(northPosition, puddleLength - 1, cubes, waterCubes)
-    const southPosition = position.clone()
-    southPosition.z -= 1
-    makeWaterCube(southPosition, puddleLength - 1, cubes, waterCubes)
-    const eastPosition = position.clone()
-    eastPosition.x += 1
-    makeWaterCube(eastPosition, puddleLength - 1, cubes, waterCubes)
-    const westPosition = position.clone()
-    makeWaterCube(westPosition, puddleLength - 1, cubes, waterCubes)
-    westPosition.x -= 1
-  }
-}
+// function makeWaterCubes(waterSource, cubes, waterCubes) {
+//   makeWaterCube(waterSource.position, 3, cubes, waterCubes)
+// }
+
+// function makeWaterCube(position, cubes, waterCubes) {
+//   //basecase - block already there, so water can't flow into it
+//   // or water is already there
+//   if (basecase) {
+//     return waterCubes
+//   }
+//   const cube = makeUnitCube(position, 0x0000ff, 1)
+//   cube.name = 'WATER'
+//   waterCubes.push(cube)
+//   const oneDown = position.clone()
+//   oneDown.y -= 1
+//   const shouldFlowDown = !cubes[toKey(oneDown)] && oneDown.y > -64
+//   if (shouldFlow){
+//     if (shouldFlowDown) {
+//       //makeWaterCube(oneDown)
+//     } else {
+//       //flow in all directions
+//       const northPosition = position.clone()
+//       northPosition.z += 1
+//       const southPosition = position.clone()
+//       southPosition.z -= 1
+//       const eastPosition = position.clone()
+//       eastPosition.x += 1
+//       const westPosition = position.clone()
+//       westPosition.x -= 1
+//       //makeWaterCube of all psoitions
+//     }
+//   }
+// }
--- client/3d/meshes/water.spec.js
+++ client/3d/meshes/water.spec.js
@@ -21,6 +21,14 @@ describe('FlowCube', () => {
     it('decreases the volume', () => {
       expect(child.volume).to.equal(3)
     })
+    it('does not decrease volume if specified', () => {
+      const childWithSameVolume = source.createChild(
+        {x: 1, y: -1, z: 0},
+        flowMap,
+        false
+      )
+      expect(childWithSameVolume.volume).to.equal(4)
+    })
     it('does not make child a source', () => {
       expect(child.isSource).to.equal(false)
     })
@@ -28,20 +36,54 @@ describe('FlowCube', () => {
       expect(child.parents).to.have.property('0,0,0')
     })
   })
+  describe('clonePosition', () => {
+    let source
+    let clone
+    before(() => {
+      source = new FlowCube(0, -62, 0, true, {}, 4)
+      clone = source.clonePosition()
+    })
+    it('creates a new object with same position', () => {
+      expect(clone).to.deep.equal(source.position)
+      expect(clone).to.not.equal(source.position)
+    })
+  })
+  describe('getAdjacentPositions', () => {
+    let source
+    let adjacentPositions
+    before(() => {
+      source = new FlowCube(0, -62, 0, true, {}, 4)
+      adjacentPositions = source.getAdjacentPositions()
+    })
+    it('gets adjacent positions on the xy plane', () => {
+      expect(adjacentPositions).to.deep.equal([
+        {x: 0, y: -62, z: 1},
+        {x: 1, y: -62, z: 0},
+        {x: 0, y: -62, z: -1},
+        {x: -1, y: -62, z: 0}
+      ])
+    })
+  })
   describe('spawnChildren', () => {
-    const cubes = {'0,-63,0': 'stuff'}
+    const cubes = {'0,-64,0': 'stuff'}
     let source
     let flowMap
     before(() => {
-      source = new FlowCube(0, -62, 0, true, {}, 4)
+      source = new FlowCube(0, -62, 0, true, {}, 2)
       flowMap = {'0,-62,0': source}
       source.spawnChildren(cubes, flowMap)
     })
-    it('flows down if possible', () => {
-      // expect(flowMap).to.have.property('0,0,0')
-    })
-    it('cannot go below y = -64', () => {
-      expect()
+    it('updates flowMap with correct values', () => {
+      expect(Object.keys(flowMap)).to.have.lengthOf(10)
+      expect(flowMap).to.have.property('0,-63,0')
+      expect(flowMap).to.have.property('1,-63,0')
+      expect(flowMap).to.have.property('-1,-63,0')
+      expect(flowMap).to.have.property('0,-63,1')
+      expect(flowMap).to.have.property('0,-63,-1')
+      expect(flowMap).to.have.property('1,-64,0')
+      expect(flowMap).to.have.property('-1,-64,0')
+      expect(flowMap).to.have.property('0,-64,1')
+      expect(flowMap).to.have.property('0,-64,-1')
     })
   })
 })
