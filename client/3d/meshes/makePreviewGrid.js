81ec45dcd490746545f94eddbee548c5b5368d6e
Stephen Ko <prodigious13@gmail.com>
2018-08-17 13:29:28 -0400

    cleaned up commented code

--- client/3d/meshes/makePreviewGrid.js
+++ client/3d/meshes/makePreviewGrid.js
@@ -1,16 +1,6 @@
 import * as THREE from 'three'
 
 function makePreviewGrid() {
-  // const start = 0
-  // const end = 0
-  // for (let i = start; i <= end; i++) {
-  //   previewBox.add(makeGridWithYaxisOffset(i))
-  // }
-  // const boxGeo = new THREE.BoxGeometry(size, size, size)
-  // const edgesGeo = new THREE.EdgesGeometry(boxGeo)
-  // const mat = new THREE.LineBasicMaterial({color: 0xffffff, width: 4})
-  // const previewGrid = new THREE.LineSegments(boxGeo, mat)
-  // return previewGrid
   const grid = makeGridWithYaxisOffset(0)
   grid.visible = false
   const toggleVisibility = function() {
