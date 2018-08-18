490e2209ef4cd3362aecb23589cdf83d6f920259
Samer Bibi <bibi.sam@gmail.com>
2018-08-18 19:05:24 -0400

    refactored

--- client/3d/controls/DragControls.js
+++ client/3d/controls/DragControls.js
@@ -139,7 +139,7 @@ THREE.DragControls = function(_camera, _domElement, _scene) {
   let color = '#' + Math.floor(Math.random() * 16777215).toString(16)
   updateAvatarInDb({x: 0, y: 0, z: 0}, worldId, yawObject.uuid, color)
   let avatars = {}
-  let avatar, disconnectRef, avatarToUpdate
+  let avatar, disconnectRef
   // event listener to create and add avatar to scene //
   const avatarsRef = db.ref(`/worlds/${worldId}/avatars`)
   avatarsRef.on('child_added', snapshot => {
@@ -159,17 +159,14 @@ THREE.DragControls = function(_camera, _domElement, _scene) {
   avatarsRef.on('child_changed', snapshot => {
     let newPosition = snapshot.val()
     if (snapshot.ref.key !== yawObject.uuid) {
-      avatarToUpdate = avatars[snapshot.ref.key]
+      let avatarToUpdate = avatars[snapshot.ref.key]
       avatarToUpdate.position.set(newPosition.x, newPosition.y, newPosition.z)
     }
     disconnectRef = db.ref(`/worlds/${worldId}/avatars/${snapshot.ref.key}`)
   })
 
-  // remove avatar from db when they dissconnect //
-  disconnectRef.onDisconnect().remove(() => {
-    deleteAvatar(_scene, avatarToUpdate)
-  })
-
+  // remove avatar from db and scene when they dissconnect //
+  disconnectRef.onDisconnect().remove()
   avatarsRef.on('child_removed', snapshot => {
     let avatarToDelete = avatars[snapshot.ref.key]
     deleteAvatar(_scene, avatarToDelete)
