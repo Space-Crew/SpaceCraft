e2ddd7fd3d1d9569ca21344bf2041b24454f23d0
Samer Bibi <bibi.sam@gmail.com>
2018-08-19 12:46:26 -0400

    changed to orbiting planets

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import * as THREE from 'three'
+import OrbitControls from 'three-orbitcontrols'
 
 // main scene //
 const scene = new THREE.Scene()
@@ -23,11 +24,131 @@ scene.add(leftLight)
 var lightAmb = new THREE.AmbientLight(0x777777)
 scene.add(lightAmb)
 
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.1)
+scene.add(ambientLight)
+
+// START PLANETS //
+const addToGroup = (orbitGroup, circle, planet) => {
+  orbitGroup.add(circle)
+  orbitGroup.add(planet)
+}
+const lineMaterial = new THREE.LineDashedMaterial({color: 'teal'})
+
+// first planet from the sun //
+const firstGeo = new THREE.CircleGeometry(2, 100)
+firstGeo.vertices.shift()
+
+const firstCircle = new THREE.Line(firstGeo, lineMaterial)
+firstCircle.rotation.x = Math.PI * 0.5
+
+const firstPlanet = new THREE.Mesh(
+  new THREE.SphereBufferGeometry(0.5, 32, 32),
+  new THREE.MeshPhongMaterial({color: 'yellow'})
+)
+firstPlanet.position.set(2, 0, 0)
+
+const firstOrbit = new THREE.Group()
+addToGroup(firstOrbit, firstCircle, firstPlanet)
+
+// second planet from the sun //
+const secondGeo = new THREE.CircleGeometry(3.5, 100)
+secondGeo.vertices.shift()
+const secondCircle = new THREE.Line(secondGeo, lineMaterial)
+secondCircle.rotation.x = Math.PI * 0.51
+const secondPlanet = new THREE.Mesh(
+  new THREE.SphereBufferGeometry(0.6, 32, 32),
+  new THREE.MeshPhongMaterial({color: 'purple'})
+)
+secondPlanet.position.set(3.5, 0, 0)
+const secondOrbit = new THREE.Group()
+addToGroup(secondOrbit, secondCircle, secondPlanet)
+
+// third planet from the sun //
+const thirdGeo = new THREE.CircleGeometry(5, 100)
+thirdGeo.vertices.shift()
+const thirdCircle = new THREE.Line(thirdGeo, lineMaterial)
+thirdCircle.rotation.x = Math.PI * 0.5
+const thirdPlanet = new THREE.Mesh(
+  new THREE.SphereBufferGeometry(0.9, 32, 32),
+  new THREE.MeshPhongMaterial({color: 'red'})
+)
+thirdPlanet.position.set(5, 0, 0)
+const thirdOrbit = new THREE.Group()
+addToGroup(thirdOrbit, thirdCircle, thirdPlanet)
+
+// combine orbits //
+const orbitDir = new THREE.Group()
+orbitDir.rotation.x = 0.2
+orbitDir.add(firstOrbit)
+orbitDir.add(secondOrbit)
+orbitDir.add(thirdOrbit)
+scene.add(orbitDir)
+
+// sun //
+const sun = new THREE.Mesh(
+  new THREE.SphereBufferGeometry(1.0, 32, 32),
+  new THREE.MeshPhongMaterial({emissive: 0xff5800, emissiveIntensity: 0.5})
+)
+
+var pointLight = new THREE.PointLight(0xffffff, 1.0, 10.0)
+sun.add(pointLight)
+scene.add(sun)
+
+// FINISHED PLANETS //
+
 // renderer //
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
 
-// add 3D text //
+// load background texture //
+const texture = new THREE.TextureLoader().load('/starrybackground.png')
+const backgroundMesh = new THREE.Mesh(
+  new THREE.PlaneGeometry(2, 2, 0),
+  new THREE.MeshBasicMaterial({map: texture})
+)
+backgroundMesh.material.depthTest = false
+backgroundMesh.material.depthWrite = false
+scene.add(backgroundMesh)
+
+// background scene //
+const backgroundScene = new THREE.Scene()
+const backgroundCamera = new THREE.Camera()
+backgroundScene.add(backgroundCamera)
+backgroundScene.add(backgroundMesh)
+
+// render function //
+
+const renderThree = () => {
+  controls.update()
+  firstOrbit.rotation.y += 0.015
+  secondOrbit.rotation.y += 0.02
+  thirdOrbit.rotation.y += 0.021
+
+  renderer.autoClear = false
+  renderer.clear()
+  renderer.render(backgroundScene, backgroundCamera)
+  renderer.render(scene, camera)
+}
+const animate = () => {
+  requestAnimationFrame(animate)
+  renderThree()
+}
+
+class Home extends Component {
+  componentDidMount() {
+    document.getElementById('homepage').appendChild(renderer.domElement)
+    animate()
+  }
+  render() {
+    return <div id="homepage" />
+  }
+}
+
+export default Home
+
+/* // add 3D text //
 const fontLoader = new THREE.FontLoader()
 fontLoader.load('/fonts/gentilis_regular.typeface.json', font => {
   let material, geom, mesh, letterWidth, x, y, z, letter
@@ -66,62 +187,20 @@ fontLoader.load('/fonts/gentilis_regular.typeface.json', font => {
     scene.add(mesh)
   }
 })
+ */
 
-// load background texture //
-const texture = new THREE.TextureLoader().load('/starrybackground.png')
-const backgroundMesh = new THREE.Mesh(
-  new THREE.PlaneGeometry(2, 2, 0),
-  new THREE.MeshBasicMaterial({map: texture})
-)
-backgroundMesh.material.depthTest = false
-backgroundMesh.material.depthWrite = false
-scene.add(backgroundMesh)
-
-// background scene //
-const backgroundScene = new THREE.Scene()
-const backgroundCamera = new THREE.Camera()
-backgroundScene.add(backgroundCamera)
-backgroundScene.add(backgroundMesh)
-
-// render function //
-let theta = 0
-let radius = 10
-
-const renderThree = () => {
-  // update 3D text rotations //
-  setTimeout(() => {
-    const timer = Date.now() * 0.0001
-    theta += 0.1
-    camera.position.x = radius * Math.sin(THREE.Math.degToRad(theta))
-    camera.position.y = radius * Math.sin(THREE.Math.degToRad(theta))
-    camera.position.z = radius * Math.cos(THREE.Math.degToRad(theta))
-    camera.lookAt(scene.position)
-    camera.updateMatrixWorld(true)
-    scene.traverse(object => {
-      object.rotation.x = timer * 2.5
-      object.rotation.y = timer * 3.5
-      object.rotation.z = timer * 2.5
-    })
-  }, 3000)
-
-  renderer.autoClear = false
-  renderer.clear()
-  renderer.render(backgroundScene, backgroundCamera)
-  renderer.render(scene, camera)
-}
-const animate = () => {
-  requestAnimationFrame(animate)
-  renderThree()
-}
-
-class Home extends Component {
-  componentDidMount() {
-    document.getElementById('homepage').appendChild(renderer.domElement)
-    animate()
-  }
-  render() {
-    return <div id="homepage" />
-  }
-}
-
-export default Home
+/* // update 3D text rotations //
+setTimeout(() => {
+  const timer = Date.now() * 0.0001
+  theta += 0.1
+  camera.position.x = radius * Math.sin(THREE.Math.degToRad(theta))
+  camera.position.y = radius * Math.sin(THREE.Math.degToRad(theta))
+  camera.position.z = radius * Math.cos(THREE.Math.degToRad(theta))
+  camera.lookAt(scene.position)
+  camera.updateMatrixWorld(true)
+  scene.traverse(object => {
+    object.rotation.x = timer * 2.5
+    object.rotation.y = timer * 3.5
+    object.rotation.z = timer * 2.5
+  })
+}, 3000) */
